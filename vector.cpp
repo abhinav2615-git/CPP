62db76ea5bbf547480f9cef27efb4db027b56ff8
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-04 10:59:29 +0530

    Add initial implementation of vector operations in vector.cpp
    - Created a new file vector.cpp
    - Implemented basic vector initialization and population with integers
    - Added a loop to print vector elements (note: corrected indexing issue)

---
+++ vector.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>v1,v2,v3,v4;
+    v1.push_back(10);
+    v1.push_back(20);
+    v1.push_back(30);
+    v2.push_back(40);
+    v2.push_back(50);
+    v2.push_back(60);
+    v2.push_back(70);
+    v3.push_back(80);
+    v3.push_back(90);
+    v3.push_back(100);
+    v4.push_back(110);
+    v4.push_back(120);
+    v4.push_back(130);
+    for(int i=0;i<=v.size();i++){
+        cout<<v[i]<<",";
+    }
+}
