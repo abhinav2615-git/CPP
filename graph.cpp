45cf6f9168996a6072d22c10d4c0ca1970447113
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-05 11:35:03 +0530

    Add basic structure for graph input in graph.cpp

---
+++ graph.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int N,E;
+    cout<<"Enter the numbers of Vertices"<<endl;
+    cin>>N;
+    cout<<"Enter the numbers of Edges"<<endl;
+    cin>>E;
+    
+}
