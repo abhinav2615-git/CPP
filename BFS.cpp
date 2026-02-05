7cbe66b430729efa5712ea1e107d19e1538953be
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-05 12:15:23 +0530

    Add BFS implementation with adjacency list representation
    - Implemented a Breadth-First Search (BFS) algorithm in C++.
    - Created an adjacency list to represent the graph.
    - Added user input for the number of vertices and edges.
    - Displayed the adjacency list and BFS traversal order.

--- BFS.cpp
+++ BFS.cpp
@@ -32,6 +32,25 @@ int main()
             cout << AdjList[i][j] << " ";
         cout << endl;
     }
+    
+    vector<int>status(N,0);
+    queue<int>Q;
+
+    Q.push(0);
+    status[0]=1;
+    cout<<"BFS order is: "<<endl;
+    while(!Q.empty()){
+        int x=Q.front();
+        Q.pop();
+        cout<<x<<",";
+        for(int j=0;j<AdjList[x].size();j++){
+            int y=AdjList[x][j];
+            if(status[y]==0){
+                status[y]=1;
+                Q.push(y);
+            }
+        }
+    }
 
     return 0;
 }
