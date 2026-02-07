27268f5fe3b633041c70640022fb4a18d5f83d58
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-07 16:35:31 +0530

    Problem no. 3

--- patterns.cpp
+++ patterns.cpp
@@ -1,37 +1,45 @@
-// Pattern 1
-// #include <iostream>
-// using namespace std;
+#include <iostream>
+using namespace std;
 
-// void pattern1(int n){
-//     for(int i =0;i<n;i++){
-//         for (int j =0;j<n;j++){
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-// }
-// int main(){
-//     int n;
-//     cout<<"Enter the numbers:";
-//     cin>>n;
-//     pattern1(n);
-// }
+void pattern1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void pattern2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void pattern3(int n){
+    for(int i = 1;i<=n;i++){
+        for(int j = 1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+}
 
-// Pattern 2
-// #include <iostream>
-// using namespace std;
+int main()
+{
+    int n;
+    cout << "Enter number: ";
+    cin >> n;
 
-// void pattern2(int n){
-//     for(int i =0;i<=n;i++){
-//         for (int j =0;j<=i;j++){
-//             cout<<"*";
-//         }
-//         cout<<endl;
-//     }
-// }
-// int main(){
-//     int n;
-//     cout<<"Enter the numbers:";
-//     cin>>n;
-//     pattern2(n);
-// }
+    pattern1(n);
+    pattern2(n);
+    pattern3(n);
+    return 0;
+}
