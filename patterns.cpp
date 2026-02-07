e3b3a477aef30c444988d604c5e5010212386b7c
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-07 16:24:00 +0530

    Pattern no. 2

--- patterns.cpp
+++ patterns.cpp
@@ -1,17 +1,37 @@
-#include <iostream>
-using namespace std;
+// Pattern 1
+// #include <iostream>
+// using namespace std;
 
-void pattern1(int n){
-    for(int i =0;i<n;i++){
-        for (int j =0;j<n;j++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
-}
-int main(){
-    int n;
-    cout<<"Enter the numbers:";
-    cin>>n;
-    pattern1(n);
-}
+// void pattern1(int n){
+//     for(int i =0;i<n;i++){
+//         for (int j =0;j<n;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter the numbers:";
+//     cin>>n;
+//     pattern1(n);
+// }
+
+// Pattern 2
+// #include <iostream>
+// using namespace std;
+
+// void pattern2(int n){
+//     for(int i =0;i<=n;i++){
+//         for (int j =0;j<=i;j++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter the numbers:";
+//     cin>>n;
+//     pattern2(n);
+// }
