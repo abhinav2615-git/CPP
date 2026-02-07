279eb3641c81abb2ee57187a79c746e606af56ec
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-07 16:59:46 +0530

    Pattern no. 6

--- patterns.cpp
+++ patterns.cpp
@@ -47,16 +47,26 @@ void pattern4(int n)
 }
 void pattern5(int n)
 {
-    for (int i = 0; i <= n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        for (int j = n - i + 1; j <= 0; j--)
+        for (int j = 0; j < n - i + 1; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+void pattern6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
         {
             cout << j;
         }
         cout << endl;
     }
 }
-
 int main()
 {
     int n;
@@ -68,5 +78,6 @@ int main()
     pattern3(n);
     pattern4(n);
     pattern5(n);
+    pattern6(n);
     return 0;
 }
