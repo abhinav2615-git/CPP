e4cb79a36bbc042aa729f7359a1d62372d8e00c8
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-09 19:53:25 +0530

    Fix pattern7 logic and add pattern8 function for improved output formatting

--- patterns.cpp
+++ patterns.cpp
@@ -75,17 +75,33 @@ void pattern7(int n)
         {
             cout << " ";
         }
-        for (int k = 0; k <= 2 * i * 1; k++)
+        for (int k = 0; k < 2 * i + 1; k++)
         {
             cout << "*";
         }
-        for (int l = 0; l <= n - i - 1; l++)
+        for (int l = 0; l < n - i - 1; l++)
         {
             cout << " ";
         }
         cout<<endl;
     }
 }
+void pattern8(int n)
+{
+    for(int i = 0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<" ";
+        }
+        for(int j=0;j<(2*n-(2*i+1));j++){
+            cout<<"*";
+        }
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        cout<<endl;
+    }
+}
+
 int main()
 {
     int n;
@@ -99,5 +115,6 @@ int main()
     pattern5(n);
     pattern6(n);
     pattern7(n);
+    pattern8(n);
     return 0;
 }
