5efee2a3ab35a783294dd4adf16e135ec06c822d
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-10 20:57:20 +0530

    Print all divisors.

--- practice.cpp
+++ practice.cpp
@@ -2,26 +2,26 @@
 using namespace std;
 
 // {
-    // vector<int>v;
-    // v.push_back(10);
-    // v.push_back(20);
-    // v.push_back(30);
-    // v.push_back(40);
-    // for(int i=0;i<v.size();i++){
-    //     cout<<v[i]<<endl;
-    // }
+// vector<int>v;
+// v.push_back(10);
+// v.push_back(20);
+// v.push_back(30);
+// v.push_back(40);
+// for(int i=0;i<v.size();i++){
+//     cout<<v[i]<<endl;
+// }
 
-    // vector<pair<int,int>>p;
-    // p.emplace_back(1,2);
-    // for(int i=0;i<p.size();i++){
-    //     cout<<p[i].first<<endl;
-    // }
+// vector<pair<int,int>>p;
+// p.emplace_back(1,2);
+// for(int i=0;i<p.size();i++){
+//     cout<<p[i].first<<endl;
+// }
 
-    // vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    // vector<int>::iterator it ;
-    // for(it= v.begin();it<=v.end();it++){
-    //     cout<<*(it)<<endl;
-    // }
+// vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+// vector<int>::iterator it ;
+// for(it= v.begin();it<=v.end();it++){
+//     cout<<*(it)<<endl;
+// }
 // }
 
 // Reverse a number
@@ -42,28 +42,42 @@ using namespace std;
 //     cout<<reverse(n)<<endl;
 // }
 
-
 // Check Palindrome
-int palindrome(int n){
-    int original=n;
-    int revNum=0;
-    int lastDig=0;
-    while(n>0){
-        lastDig=n%10;
-        revNum=(revNum*10)+lastDig;
-        n=n/10;
+// int palindrome(int n){
+//     int original=n;
+//     int revNum=0;
+//     int lastDig=0;
+//     while(n>0){
+//         lastDig=n%10;
+//         revNum=(revNum*10)+lastDig;
+//         n=n/10;
+//     }
+//     if(revNum==original){
+//         cout<<"The number is palindrome."<<endl;
+//     }
+//     else{
+//         cout<<"Number is not a palindrome."<<endl;
+//     }
+//     return revNum;
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter the number to be checked:"<<endl;
+//     cin>>n;
+//     cout<<palindrome(n)<<endl;
+// }
+
+// Print all Divisors
+void divisors(int n){
+    for(int i=0;i<=sqrt(n);i++){
+        if(n%i==0){
+            cout<<i<<" "<<(n/i)<<endl;
+        }
     }
-    if(revNum==original){
-        cout<<"The number is palindrome."<<endl;
-    }
-    else{
-        cout<<"Number is not a palindrome."<<endl;
-    }
-    return revNum;
 }
 int main(){
     int n;
-    cout<<"Enter the number to be checked:"<<endl;
+    cout<<"Enter the number"<<endl;
     cin>>n;
-    cout<<palindrome(n)<<endl;
+    divisors(n);
 }
