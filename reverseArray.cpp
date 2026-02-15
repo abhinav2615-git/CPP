b54e6a1629ca8526141a77ef73e434dc5080b605
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-15 16:12:13 +0530

    Reverse the array using recursion

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void reverse(int i, vector<int> &v1, int n)
+{
+    if (i >= n / 2)
+    {
+        return;
+    }
+    swap(v1[i], v1[n - i - 1]);
+    reverse(i + 1, v1, n);
+}
 int main()
 {
     vector<int> v1;
@@ -18,13 +27,17 @@ int main()
     {
         cout << x << " ";
     }
-    cout<<endl<<"Reversed array"<<endl;
-    for(int i=0;i<=n/2;i++){
-        swap(v1[i],v1[n-1]);
-        n--;
+    cout << endl<< "Reversed array" << endl;
+    reverse(0,v1,n);
+    for(int x:v1){
+        cout<<x<<" ";
     }
-    for (int x : v1)
-    {
-        cout << x << " ";
-    }
+    // for(int i=0;i<=n/2;i++){
+    //     swap(v1[i],v1[n-1]);
+    //     n--;
+    // }
+    // for (int x : v1)
+    // {
+    //     cout << x << " ";
+    // }
 }
