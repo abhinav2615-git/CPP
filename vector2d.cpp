64c8de2efcf2ae98b05083caa1fb395ff2e8c3db
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-19 12:43:35 +0530

    2d vector and pair syntax

---
+++ vector2d.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+int main(){
+    // vector<vector<int>>vv(10,vector<int>(5,10));
+    // for(int i=0;i<vv.size();i++){
+    //     for(int j=0;j<vv[i].size();j++){
+    //         cout<<vv[i][j]<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    
+    pair<int,string>p;
+    p={10,"Abhinav"};
+    cout<<p.first;
+    cout<<" ";
+    cout<<p.second;
+}
