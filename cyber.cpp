c3b0e4e35195db09d0bedf3b4d8c3ec8592a5707
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-02-23 12:15:24 +0530

    Minor changes

--- cyber.cpp
+++ cyber.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 string bitStuff(string str){
     int count = 0;
-
     for(int i = 0; i < str.length(); i++){
         if(str[i] == '1'){
             count++;
@@ -12,20 +11,17 @@ string bitStuff(string str){
         else{
             count = 0;
         }
-
         if(count == 5){
             str.insert(i+1, "0");
             count = 0;
             i++;
         }
     }
-
     return str;
 }
 
 int main(){
     string str;
-
     cout<<"Enter the string in bits 0 and 1"<<endl;
     cin>>str;
     for(char c : str){
@@ -34,10 +30,7 @@ int main(){
             return 0;
         }
     }
-
     string result = bitStuff(str);
-
     cout<<"After bit stuffing: "<<result<<endl;
-
     return 0;
 }
