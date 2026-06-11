9c7f2d5808278037d4272764a58f9273464c0ab4
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-06-11 19:30:18 +0530

    Non-Parameterised and Parameterised constructors.

--- oops.cpp
+++ oops.cpp
@@ -9,9 +9,16 @@ private:
     int pincode;
 
 public:
+    // Non-parameterized Constructor
     Teacher(){
         dept = "Computer Science";
         cout<<"Department allotted : "<<dept<<endl;
+    }
+
+    // Parameterised Constructor
+    Teacher(string n, int i){
+        name = n;
+        id = i;
     }
     string name;
     int id;
@@ -20,16 +27,14 @@ public:
         salary = s;
         return salary;
     }
+    void info(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Id : "<<id<<endl;
+    }
 };
 
 int main(){
-    Teacher t1;
-    cout<<"Enter the name of the teacher:"<<endl;
-    cin>>t1.name;
-    cout<<"Enter the id of the teacher:"<<endl;
-    cin>>t1.id;
-
-    cout<<"The details of the teacher are:"<<endl;
-    cout<<t1.name<<endl<<t1.id<<endl;
+    Teacher t1("Abhinav Gupta", 33);
+    t1.info();
     cout<<"The salary is: "<<t1.setSalary(25000);
 }
