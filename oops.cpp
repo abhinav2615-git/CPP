f06050f22fa9b292db4e59942b303eee19927b62
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-07-02 15:53:35 +0530

    Inheritance concept-1

--- oops.cpp
+++ oops.cpp
@@ -45,11 +45,61 @@ public:
     }
 };
 
+//Inheritance
+class Person{
+public:
+    int age;
+    string name;
+
+    void getinfo(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age : "<<age<<endl;
+    }
+
+    Person(){
+        cout<<"Object Created for Person"<<endl;
+    }
+};
+
+class Student : public Person{
+public:
+    int rollno;
+
+    void getinfo(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age : "<<age<<endl;
+        cout<<"Roll Number : "<<rollno<<endl;
+    }
+
+    Student(){
+        cout<<"Object Created for Student."<<endl;
+    }
+};
+
 int main()
 {
-    Teacher t1("Abhinav Gupta", 33);
-    t1.info();
-    cout << "The salary is: " << t1.setSalary(25000)<<endl;
-    Teacher t2(t1);
-    t2.info();
+    // Teacher t1("Abhinav Gupta", 33);
+    // t1.info();
+    // cout << "The salary is: " << t1.setSalary(25000)<<endl;
+    // Teacher t2(t1);
+    // t2.info();
+
+    //Inheritance
+
+    Person p1;
+    p1.name = "Abhi";
+    p1.age = 21;
+    p1.getinfo();
+
+    Student s1;
+    s1.name = "Abhinav";
+    s1.age = 20;
+    s1.rollno = 33;
+    s1.getinfo();
+
+    p1.name = "Changed";
+    cout << p1.name;
+    cout << s1.name;
+
+    return 0;
 }
