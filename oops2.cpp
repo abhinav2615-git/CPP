68463f3434a2b0bb970a4100229f49f4eb09a19c
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-07-06 08:14:05 +0530

    Abstract class and Static keyword

--- oops2.cpp
+++ oops2.cpp
@@ -17,9 +17,40 @@ public:
     }
 };
 
+// Abstract Class
+class Shape{
+public:
+    virtual void draw() = 0; //Pure Abstract Function
+};
+
+class Circle : public Shape{
+public: 
+    void draw(){
+        cout<<"Draw a circle."<<endl;
+    }
+};
+
+//Static Keyword
+void fun(){
+     static int x = 0;
+    cout<<"X : "<<x<<endl;
+    x++;
+}
+
 int main(){
-    Parent p1;
-    p1.getinfo();
-    Child c1;
-    c1.getinfo();
+    // Parent p1;
+    // p1.getinfo();
+    // Child c1;
+    // c1.getinfo();
+
+    //Abstract Class
+    // Circle c1;
+    // c1.draw();
+
+    //Static Keyword
+    fun();
+    fun();
+    fun();
+    fun();
+    fun();
 }
