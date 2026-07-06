fbbf968dcab50d8bbca9999ef68e150bbc23589f
Abhinav Gupta <gupta.abhinav1504@gmail.com>
2026-07-06 08:33:56 +0530

    Pointers To Pointers

--- pointers.cpp
+++ pointers.cpp
@@ -4,8 +4,17 @@ using namespace std;
 int main(){
 
     //Pointers
-    int x = 12;
-    int* ptr = &x;
-    cout<<ptr<<endl;
-    cout<<&x<<endl;
+    // int x = 12;
+    // int* ptr = &x;
+    // cout<<ptr<<endl;
+    // cout<<&x<<endl;
+
+    //Pointers to Pointers
+    int y = 10;
+    int* ptr = &y;
+    int** ptr2 = &ptr;
+
+    cout << &y << endl;
+    cout << ptr << endl;
+    cout << ptr2 << endl;
 }
